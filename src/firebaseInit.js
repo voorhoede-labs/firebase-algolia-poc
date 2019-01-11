13aa1fff84b98e6191a246556113f01c6f3e6c47
Jesse Eikema <jesse@voorhoede.nl>
2019-01-11 12:28:49 +0100

    remove anoying firestore warning about timestamps

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,5 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore'
 import firebaseConfig from './firebaseConfig'
+
 const firebaseApp = firebase.initializeApp(firebaseConfig)
+const settings = { timestampsInSnapshots: true };
+firebaseApp.firestore().settings(settings);
+
 export default firebaseApp.firestore()
